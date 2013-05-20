2c5a891784a151d82c28c623a5fd5628233a4fd2
Aniket Panse <aniketpanse@gmail.com>
2013-05-20 17:10:30 +0530

    initial commit

---
+++ news/bitelia.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export http_proxy=http://f2009619:hkhg874@10.1.9.20:8080/
+
+uri=http://feeds.hipertextual.com/bitelia
+lines=4
+titlenum=
+if [[ "$uri" == "" ]]; then
+        echo "No URI specified, cannot continue!" >&2
+        echo "Please read script for more information" >&2
+else
+        #Set defaults if none specified
+        if [[ $lines == "" ]]; then lines=25 ; fi
+        if [[ $titlenum == "" ]]; then titlenum=2 ; fi
+
+        #The actual work
+        curl -s --connect-timeout 30 $uri |\
+        sed -e 's/<\/title>/\n/g' |\
+        grep -o '<title>.*' |\
+        sed -e 's/<title>//' |\
+        head -n $(($lines + $titlenum)) |\
+        tail -n $(($lines))
+fi
